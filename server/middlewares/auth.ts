854b3cbed40f679c57409b085bf426bdcb29154f
Nikhil Bindal <bindal.n@northeastern.edu>
2023-12-04 09:57:27 -0500

    aded stock module and middleware

---
+++ server/middlewares/auth.ts
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const jwtSecret = process.env.JWT_SECRET;
+
+export const verifyToken = ({ token }) => {
+    try {
+        const isCustomAuth = token.length < 500;
+        let contentDecoded;
+        let userId;
+
+        if (token && isCustomAuth) {
+            contentDecoded = jwt.verify(token, jwtSecret);
+            userId = contentDecoded?.id;
+        } else {
+            contentDecoded = jwt.decode(token);
+            userId = contentDecoded?.sub;
+        }
+
+        if (!userId) {
+            return { error: 'Invalid Token' };
+        }
+
+        return { userId };
+    } catch (error) {
+        return { error: 'Invalid Token' };
+    }
+};
